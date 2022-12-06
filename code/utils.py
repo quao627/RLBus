6f90ccf0b78dce723de0d1a30921c8c5bf166713
Junghoon Cho <cr2622@snu.ac.kr>
2022-12-05 22:58:22 -0500

    fix timetable, alight, and arrival table

--- code/utils.py
+++ code/utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+seed=0
+np.random.seed(seed)
 
 # number of bus
 N_BUS = 6
@@ -13,12 +15,24 @@ from params import N_STATION, CAPACITY
 horizon = 3 * 60 * 60 # 3 hours
 travel_time_step = 5 * 60 # travel times changes every 5 minutes
 
-# total number of passengers
-n_passenger = 100
 
 # passenger arrival and alight rate
-pax_arrive_rate = n_passenger/horizon*3600
-pax_avg_inveh_time = 5*60 # 5 minutes
+avg_pax_arrive_rate = 3 # p (sec/pax)
+std_pax_arrive_rate = 5
+
+# pax_arrive_rate = [1/avg_pax_arrive_rate] * N_STATION # lambda pax/sec
+pax_arrive_rate = [(np.exp(-0.5*((i-int(N_STATION)*0.5)/std_pax_arrive_rate)**2)* 1/ avg_pax_arrive_rate) for i in range(N_STATION)] # lambda pax/sec
+# pax_arrive_rate = [1/(np.exp(-0.5*((i-int(N_STATION)*0.5)/std_pax_arrive_rate)**2)* avg_pax_arrive_rate) for i in range(N_STATION)] # lambda pax/sec
+
+pax_avg_inveh_time = 5 * 60 # 5 minutes
+
+# total number of passengers per each station
+n_passenger = [horizon * pax_arrive_rate[i] for i in range(N_STATION)]
+
+# average travel time to scale the travel time table
+avg_travel_time = 4*60 # 4 minutes
+min_travel_time = 2*60 # 2 minutes
+max_travel_time = 10*60 # 10 minutes
 
 # creating travel time table with poisson distribution
 def gen_travel_time_table(n_station, horizon, travel_time_step, sigma=10, poi_lam=0.5, seed=0):
@@ -26,7 +40,8 @@ def gen_travel_time_table(n_station, horizon, travel_time_step, sigma=10, poi_la
     tt_table = np.zeros((n_station, int(horizon/travel_time_step)))
     for i in range(n_station):
         for j in range(int(horizon/travel_time_step)):
-            tt_table[i][j] = np.exp(-0.5*((j-int(horizon/travel_time_step)*0.5)/sigma)**2) + np.random.poisson(poi_lam)*0.5
+            tt_table[i][j] = avg_travel_time * (np.exp(-0.5*((j-int(horizon/travel_time_step)*0.5)/sigma)**2) + np.random.poisson(poi_lam)*0.5)
+    tt_table = np.clip(tt_table, min_travel_time, max_travel_time)
     return tt_table
 
 table_travel_time = gen_travel_time_table(N_STATION, horizon, travel_time_step)
@@ -47,33 +62,30 @@ plt.plot(np.mean(table_travel_time, axis=1))
 # value -> the time step the passenger arrives at the station i (based on Poisson distribution)
 def gen_pax_arrive(n_station, horizon, n_passenger, pax_arrive_rate, seed=0):
     np.random.seed(seed)
-    pax_arrive_table = np.zeros((n_station, n_passenger))
-    for j in range(n_passenger):
-        i = np.random.randint(n_station)
-        if j == 0:
-            pax_arrive_table[i][j] += np.random.poisson(pax_arrive_rate)
-        else:
-            pax_arrive_table[i][j] += np.sum(pax_arrive_table.transpose()[j-1]) + np.random.poisson(pax_arrive_rate)
-        if pax_arrive_table[i][j]>horizon:
-            pax_arrive_table[i][j] = horizon
+    pax_arrive_table = np.zeros((n_station, int(n_passenger)))
+    for i in range(n_station):
+        arrival_time = 0
+        j = 0
+        while arrival_time < horizon:
+            arrival_time += np.random.exponential(1/pax_arrive_rate[i])
+            pax_arrive_table[i][j] = arrival_time
+            j += 1
+    pax_arrive_table[pax_arrive_table == 0] = np.inf
     return pax_arrive_table
-pax_arrive_table = gen_pax_arrive(N_STATION, horizon, n_passenger, pax_arrive_rate)
+pax_arrive_table = gen_pax_arrive(N_STATION, horizon, 5000, pax_arrive_rate)
 
 # generating table for number of alighting passengers at each station at each time step
 # i -> for each station
-# j -> the number of passsengers
-# value -> the time step the passenger alighting at the station i (based on Poisson distribution)
-def gen_pax_alight(n_station, horizon, pax_arrive_table, seed=0, pax_avg_inveh_time=5*60, sigma=60):
+# j -> 5 minutes time step
+# value -> 1/(N_STATION-1) 
+def gen_pax_alight(n_station, horizon, n_passenger, pax_arrive_table, seed=0, pax_avg_inveh_time=5*60, sigma=60):
     np.random.seed(seed)
-    pax_alight_table = np.zeros((n_station, n_passenger))
-    for j in range(n_passenger):
-        board_station = np.nonzero(pax_arrive_table.transpose()[10])[0].item()
-        i = np.random.randint(board_station, n_station)
-        pax_alight_table[i][j] += np.sum(pax_arrive_table.transpose()[j]) + np.random.normal(pax_avg_inveh_time, sigma)
-        if pax_alight_table[i][j]>horizon:
-            pax_alight_table[i][j] = horizon
+    pax_alight_table = np.zeros((n_station, int(horizon/travel_time_step)))
+    for i in range(n_station):
+        for j in range(int(horizon/travel_time_step)):
+            pax_alight_table[i][j] += 1/(n_station-i)
     return pax_alight_table
-pax_alight_table = gen_pax_alight(N_STATION, horizon, pax_arrive_table)
+pax_alight_table = gen_pax_alight(N_STATION, horizon, 5000, pax_arrive_table)
 
 sns.heatmap(pax_arrive_table)
 sns.heatmap(pax_alight_table)
